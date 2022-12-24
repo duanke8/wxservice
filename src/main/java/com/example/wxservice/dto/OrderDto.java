b033a0fad5041cc59b55360c9d55816a5e53d773
duanke8 <260160158.qq.com>
2022-12-24 23:13:03 +0800

    调试页面

---
+++ src/main/java/com/example/wxservice/dto/OrderDto.java
@@ -0,0 +1,24 @@
+package com.example.wxservice.dto;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+@TableName("order_info")
+public class OrderDto {
+
+    @TableId(type = IdType.AUTO)
+    private long id;
+    private String orderId;
+    private String orderInfo;
+    private String orderImages;
+    private Date createTime;
+    private Date updateTime;
+    private String createUserId;
+
+
+}
